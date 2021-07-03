d718944ddc0ae16ea899e82325e73c01d3e098eb
savioramon <savioramon12@gmail.com>
2021-07-03 14:16:05 -0300

    configurando redux saga

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,20 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import moedasSelecionadas from "./Conversor/Conversor.reducer";
+
+import createSagaMiddleware from "redux-saga";
+import sagasRoot from "./sagas";
 
 const rootReducer = combineReducers({
     moedasSelecionadas
 })
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+
+sagaMiddleware.run(sagasRoot);
+
 
 export default store;
