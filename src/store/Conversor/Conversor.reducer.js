5bcb07976a878c69c267bc71db66dad3b79ed031
savioramon <savioramon12@gmail.com>
2021-07-07 16:28:24 -0300

    primeiros passos conversor

--- src/store/Conversor/Conversor.reducer.js
+++ src/store/Conversor/Conversor.reducer.js
@@ -1,8 +1,21 @@
 
+const convertendo = (valor, valorBase)=>{
+    let conversao = "";
+    let numero = parseFloat(valor);
+	if(typeof(numero) === "number" && numero >= 0) {
+		conversao = numero * valorBase;
+	}
+
+    conversao = parseFloat(conversao).toFixed(2).replace(".", ",");
+    
+    return conversao;
+    
+}
 
 const stateInicial = {
     moedaUm: "USD",
-    moedaDois: "BRL"
+    moedaDois: "BRL",
+    resultado: ""
 }
 
 
@@ -11,8 +24,16 @@ export default function(state=stateInicial, action) {
     switch(action.type) {
 
         case "MOEDA":
-            return action.payload;
 
+            return action.payload;
+        case "CONVERSAO":
+            const resultado = convertendo(action.payload.valor, action.payload.valorBase);
+            
+            return {
+                ...state,
+                resultado
+            };
+        
         default:
             return state;
 
